3cbb51670d0a944da8a89a31b0e6a5f999b93720
Marina Ivanova <marina.ivanova@ppb.com>
2025-07-16 16:22:59 +0100

    Tests and docker compose

---
+++ src/test/java/com/assignment/eagleBank/integrationTests/UserRepositoryTests.java
@@ -0,0 +1,34 @@
+package com.assignment.eagleBank.RepositoryTests;
+
+import com.assignment.eagleBank.TestUtils;
+import com.assignment.eagleBank.entity.User;
+import com.assignment.eagleBank.repositories.UserRepository;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import static org.assertj.core.api.Assertions.assertThat;
+
+@ExtendWith(SpringExtension.class)
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+public class UserRepositoryTests {
+    @Autowired
+    private UserRepository userRepository;
+    @Autowired
+    private TestEntityManager entityManager;
+    @Test
+    void whenFindByEmail_thenReturnCorrectUser() {
+        User user = TestUtils.createNewUser(TestUtils.createNewAddress());
+        entityManager.persistAndFlush(user);
+
+        User returnedUser = userRepository.findByEmail(user.getEmail()).orElse(null);
+
+        assertThat(returnedUser).isNotNull();
+        assertThat(returnedUser.getEmail()).isEqualTo(user.getEmail());
+        assertThat(returnedUser.getName()).isEqualTo(user.getName());
+    }
+}
